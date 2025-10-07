39ef2c3ac357b894a5d615e62eafb323fd564a5d
Drixx234 <andrycklaguna2024@gmail.com>
2025-10-07 08:21:47 -0600

    Add files via upload

---
+++ src/test/java/finalBoss/pruebaFinal/PruebaFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package finalBoss.pruebaFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
